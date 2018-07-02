9f5d2e3791a0cb670fdc92ea40ed67b71744851d
harshavardhan Reddy <o.harshavardhan@gmail.com>
2018-07-01 23:22:43 -0400

    created a component and added some functionality

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  hello: string;
+  name: string;
+  age: number;
+  email: string;
+  address: Address;
+  hobbies: any;
+
+  constructor() {
+    console.log('constructor ran');
+  }
+
+  ngOnInit() {
+    console.log('ngon init ran');
+
+
+    this.name = 'Harsha';
+    this.age = 30;
+    this.address = {
+      street : '50 xyz street',
+      city : 'Atlanta',
+      state: 'Georgia'
+    };
+    this.hobbies = ['write code', 'play games', 'read articles'];
+    this.hello = 'hello';
+  }
+
+  onClick() {
+    this.name = 'Ram';
+    this.hobbies.push('New Hobbie');
+  }
+
+  addHobby(hobby) {
+    console.log(hobby);
+    this.hobbies.unshift(hobby);
+    return false;
+  }
+
+  deleteHobby(hobby) {
+    // console.log(hobby);
+    for (let i = 0; i < this.hobbies.length; i++) {
+      if (this.hobbies[i] === hobby) {
+        this.hobbies.splice(i, 1);
+      }
+
+    }
+    // this.hobbies.pop(hobby);
+  }
+
+}
+
+
+interface Address {
+    street: string;
+    city: string;
+    state: string;
+  }
